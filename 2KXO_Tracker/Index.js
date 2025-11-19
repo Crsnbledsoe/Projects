2f13e852451f4afac82eb31dd24722332bffdecc
Carson Bledsoe <crsnbledsoe@gmail.com>
2025-11-19 17:48:22 -0500

    2kxo changes

--- 2KXO_Tracker/Index.js
+++ 2KXO_Tracker/Index.js
@@ -4,32 +4,8 @@ const win = statForm['win'];
 const fighter1 = statForm['fighter1'];
 const fighter2 = statForm['fighter2'];
 
-const result = [
-    {
-        win: "w",
-        fighter1: "Ahri",
-        fighter2: "Yasuo",
-    },
-];
+document.getElementById("saveBtn").addEventListener("click", myAlert);
 
-
-const addResult = (win, fighter1, fighter2) => {};
-
-const createResultEvent = ({win, fighter1, fighter2})=> {
-    const resultDiv = document.createElement('div');
-    const win = document.createElement('h2');
-    const fighter1 = document.createElement('p');
-    const fighter2 = document.createElement('p');
-
-
-    win.innerText = "Result: " + win
-    fighter1.innerText = "Result " + fighter1;
-    fighter2.innerText = "Result " + fighter2;
-
-    resultDiv.append(win, fighter1, fighter2);
-    resultsContainer.appendChild(resultDiv);
-
+function myAlert() {
+    alert("Match Saved!");
 }
-
-
-students.forEach(createResultEvent)
