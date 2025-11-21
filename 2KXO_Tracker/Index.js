ae7155b40ddc09ea94c3783ffb35356f416a601b
Carson Bledsoe <crsnbledsoe@gmail.com>
2025-11-20 21:23:26 -0500

    JS Updates

--- 2KXO_Tracker/Index.js
+++ 2KXO_Tracker/Index.js
@@ -4,8 +4,13 @@ const win = statForm['win'];
 const fighter1 = statForm['fighter1'];
 const fighter2 = statForm['fighter2'];
 
+
+
 document.getElementById("saveBtn").addEventListener("click", myAlert);
 
 function myAlert() {
     alert("Match Saved!");
 }
+
+const matches = document.querySelectorAll("#statForm");
+console.log(matches)
