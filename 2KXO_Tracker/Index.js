45cc16c8c626035dae70204990876c774d308750
Carson Bledsoe <crsnbledsoe@gmail.com>
2025-11-26 15:34:26 -0500

    Display results working!

--- 2KXO_Tracker/Index.js
+++ 2KXO_Tracker/Index.js
@@ -1,16 +1,22 @@
-const statForm = document.getElementById('statForm');
-const resultsContainer = document.querySelector(".results")
-const win = statForm['win'];
-const fighter1 = statForm['fighter1'];
-const fighter2 = statForm['fighter2'];
+//const statForm = document.getElementById('statForm');
+//const resultsContainer = document.querySelector(".results")
+//const win = statForm['win'];
+const fighter1 = document.getElementById("fighter1");
+//const fighter2 = statForm['fighter2'];
+
 
 
 
 document.getElementById("saveBtn").addEventListener("click", myAlert);
 
-function myAlert() {
+function myAlert(e) {
+    e.preventDefault();
+    localStorage.setItem("pc1",fighter1.value)
+    const pc1 = localStorage.getItem("pc1")
+    console.log(pc1)
+    document.getElementById("results").textContent = pc1
     alert("Match Saved!");
 }
 
-const matches = document.querySelectorAll("#statForm");
-console.log(matches)
+//const matches = document.querySelectorAll("#statForm");
+//console.log(matches)
