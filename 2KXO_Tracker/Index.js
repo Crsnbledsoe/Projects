eb22c689f818a2e0ac1eba8de2a9903352fce9e9
Carson Bledsoe <crsnbledsoe@gmail.com>
2025-12-12 15:55:38 -0500

    js changes and such

--- 2KXO_Tracker/Index.js
+++ 2KXO_Tracker/Index.js
@@ -10,12 +10,35 @@ const win = document.getElementById("win");
 
 
 document.getElementById("saveBtn").addEventListener("click", myAlert);
+//document.addEventListener('DOMContentLoaded', refLocStor());
 
 function myAlert(e) {
     e.preventDefault();
-    const matchData = [
-        win.value,fighter1select.value,fighter2select.value
-    ];
+
+    //Loads any existing matches saved in local storage or creates a new/empty array titled 'matches'
+    const matches = JSON.parse(localStorage.getItem("matches")) || [];
+
+    //add a new match
+    const newMatch = {
+        win: win.value,
+        fighter1: fighter1select.value,
+        fighter2: fighter2select.value,
+    };
+    
+    //push newMatch to matches array
+    matches.push(newMatch);
+
+    displayMatches();
+}
+
+function displayMatches(match) {
+    const para = document.createElement("p");
+    para.textContent = `${match.win} | ${match.fighter1select} & ${match.fighter2select}`;
+    document.getElementById("results").appendChild(para);
+}
+/*
+    localStorage.setItem("matches",JSON.stringify(matches))
+
     localStorage.setItem("match",JSON.stringify(matchData));
     //const match = localStorage.getItem("match")
     const match = JSON.parse(localStorage.getItem("match"))
@@ -25,7 +48,13 @@ function myAlert(e) {
     document.getElementById("results").appendChild(para)
     //document.getElementById("results").textContent = match
     alert("Match Saved!");
-}
+}*/
 
+/*function refLocStor() {
+ const match = JSON.parse(localStorage.getItem("match"))
+    console.log(match)
+    const para = document.createElement("p");
+    para.textContent = match
+    document.getElementById("results").appendChild(para)}
 //const matches = document.querySelectorAll("#statForm");
-//console.log(matches)
+//console.log(matches)};*/
