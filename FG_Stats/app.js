c4b637557482d3e371fcc806ba5c29de640e4e6e
Carson Bledsoe <crsnbledsoe@gmail.com>
2026-05-29 00:44:46 -0400

    learning express

--- FG_Stats/app.js
+++ FG_Stats/app.js
@@ -5,9 +5,16 @@ const app = express();
 //listen for requests
 app.listen(3000);
 app.get('/', (req, res) => {
-    res.send('<p>Home Page</p>') //.send method infers the content type and status code
+   res.sendFile('./HTML/2XKOT.html', {root: __dirname})
 });
 
+//404
+app.use((req, res) => {
+    res.sendFile('./HTML/404.html', {root: __dirname})
+})
+//app.get('/2XKOT', (req, res) => {
+   // res.send('<p>Home Page</p>') 
+//});
 fs.writeFile('./test1.txt', 'hello world', (err) => {
 
     if (err) {
