5721001843903388a0bac9b06efe0afa291d5780
Carson Bledsoe <crsnbledsoe@gmail.com>
2026-08-03 09:54:49 -0400

    fixing git

--- FG_Stats-backup/db/connection.js
+++
@@ -1,6 +0,0 @@
-//pulls in bettersqlite3 and the variable database becomes a sort of blueprint we can use to build databases
-const Database = require('better-sqlite3');
-//keyword 'new' actually builds database at the file location listed
-const db = new Database('Data/stats.db');
-//allows other files(like app.js) to use the db variable
-module.exports = db;
